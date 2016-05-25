3ab9f19cb69408aea07478460589d59df65f3cde
Jonathan Alexander <jonathandalexander@gmail.com>
2016-05-25 11:27:06 -0400

    Done. Added .prepend method."

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-	if array.size == 1
-		array.first
-	elsif array.size == 2
-		array.join(" and ")
-	else 
-		last = array.pop
-		array.join(', ') << ", and #{last}"
-	end
+  if array.size == 1 
+    array.join
+  elsif array.size == 2
+    array.join(' and ')
+  elsif array.size > 2
+    array.last.prepend('and ')
+    array.join(', ')
+  end
 end 
